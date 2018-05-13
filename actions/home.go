898de054f099cf577bf7d597422048e5fbc46b85
jcarley <jeff.carley@gmail.com>
2018-05-13 06:47:02 -0500

    Demo clone handler

--- actions/home.go
+++ actions/home.go
@@ -7,3 +7,14 @@ import "github.com/gobuffalo/buffalo"
 func HomeHandler(c buffalo.Context) error {
 	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
 }
+
+func CloneRepoHandler(c buffalo.Context) error {
+
+	cmd := NewCloneCommand(repoUrl, localPath)
+	err := cmd.Run()
+	if err != nil {
+		return c.Error(400, err)
+	}
+
+	return nil
+}
